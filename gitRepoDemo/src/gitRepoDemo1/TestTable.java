f12e3c240ebcbeb323d96ce431d0ec4646d18998
Rajeshwar <rajeshwarspc@gmail.com>
2023-02-20 13:49:25 +0530

    added testTable file

---
+++ gitRepoDemo/src/gitRepoDemo1/TestTable.java
@@ -0,0 +1,17 @@
+package gitRepoDemo1;
+
+public class TestTable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int num = 2;
+		System.out.println("Printing Table of "+num+": ");
+		for(int i = 1; i<=10; i++)
+		{
+			System.out.println(num+" * "+i+" = "+(num*i));
+		}
+
+	}
+
+}
