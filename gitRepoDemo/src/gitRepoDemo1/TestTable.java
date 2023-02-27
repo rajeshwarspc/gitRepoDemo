4696ce8863612483bedce6c4159cdce6641bbfdd
Rajeshwar <rajeshwarspc@gmail.com>
2023-02-27 09:01:40 +0530

    updated TestTable.java

--- gitRepoDemo/src/gitRepoDemo1/TestTable.java
+++ gitRepoDemo/src/gitRepoDemo1/TestTable.java
@@ -8,7 +8,7 @@ public class TestTable {
 	public void testTable1() {
 		// TODO Auto-generated method stub
 		
-		int num = 2;
+		int num = 3;
 		System.out.println("Printing Table of "+num+": ");
 		for(int i = 1; i<=10; i++)
 		{
